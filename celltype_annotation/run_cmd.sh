02bba1c7fd62776e846ca503a218f7ac52063d97
ase12345636 <ase12345636@gmail.com>
2026-03-25 02:02:28 +0800

    Add draw plot chart code into run_cmd.sh

--- celltype_annotation/run_cmd.sh
+++ celltype_annotation/run_cmd.sh
@@ -2,3 +2,13 @@ bash run_benchmark.sh \
     --dataset_path /Group16T/common/ccuc/scLightGAT/scLightGAT_data \
     --save_path /Group16T/common/ccuc/Workspace/celltype_annotation/result \
     --run_times 5
+
+python3 analyze/plot_benchmark_methods_bar_charts_csv.py \
+    --results_dir /Group16T/common/ccuc/Workspace/celltype_annotation/result \
+    --sclightgat_dir /Group16T/common/ccuc/scLightGAT/sclightgat_exp_results \
+    --out_dir /Group16T/common/ccuc/Workspace/celltype_annotation/result/figures
+
+python3 analyze/plot_subtype_methods_bar_charts_csv.py \
+    --results_dir /Group16T/common/ccuc/Workspace/celltype_annotation/result \
+    --sclightgat_dir /Group16T/common/ccuc/scLightGAT/sclightgat_exp_results \
+    --out_dir /Group16T/common/ccuc/Workspace/celltype_annotation/result/figures/subtype
